f4551135ba5fc66bea49e5ae72e843bb1b818502
Rahul Yadav <rahulcyadav@gmail.com>
2019-05-31 22:06:55 +0530

    bugfix

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,12 +1,10 @@
 import { all, fork } from 'redux-saga/effects';
-import { watchCreateInterest, watchFetchLocations } from './interests';
+import {  watchFetchLocations,watchFetchSports  } from './interests';
 import { watchLoginRequest, watchRegisterUser } from './login';
-import { watchFetchLocations, watchFetchSports } from './interests';
 import { watchFetchRooms } from './rooms';
 
 const rootSaga = function*() {
     yield all([
-        fork(watchCreateInterest),
         fork(watchLoginRequest),
         fork(watchRegisterUser),
         fork(watchFetchLocations),
